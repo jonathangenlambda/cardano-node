85f0f3a70ce1337e0e2d5e022b9d6c3dc8a65008
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2022-09-01 12:56:51 +0100

    Remove redundant packages arguments

--- nix/workbench/shell.nix
+++ nix/workbench/shell.nix
@@ -78,8 +78,6 @@ in project.shellFor {
 
   inherit withHoogle;
 
-  packages = ps: builtins.attrValues (haskellLib.selectProjectPackages ps);
-
   tools = {
     haskell-language-server = {
       version = "latest";
